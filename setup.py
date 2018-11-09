fd4e894f99d26abffd59adb96e84c58f49e5b985
Rowland Ogwara <rogwara@uchicago.edu>
2018-11-09 17:19:34 -0600

    setup py fix

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name="GDC queues client",
     version="1.0.0",
-    description=find_packages(exclude={'tests', 'docs'}),
+    description="Generic Queue Client",
+    packages=find_packages(exclude={'tests', 'docs'}),
     install_requires=[
         "pika==0.12.0",
         "requests>=2.7"
