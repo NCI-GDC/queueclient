a8d30f7e748b69c4120d34fb2bc1983c90a93ccd
Jason Stiles <99777116+stilesj-uchicago@users.noreply.github.com>
2023-05-09 09:33:44 -0400

    Loosen restrictions

--- setup.py
+++ setup.py
@@ -9,6 +9,6 @@ setup(
     description="Queue Client Facade: A generalized API that allows for connections to multiple queue implementations.",
     license="Apache",
     packages=find_packages(exclude=("tests", "docs")),
-    install_requires=["pika~=1.1.0", "requests>=2,<3", "simplejson>=3,<4"],
+    install_requires=["pika~=1.1", "requests>=2,<3", "simplejson>=3,<4"],
     setup_requires=["setuptools_scm~=6.4"],
 )
