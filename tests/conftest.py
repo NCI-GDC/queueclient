e1795079c4ba16c9412a22c15027ad49e78d91df
Rowland Ogwara <rogwara@uchicago.edu>
2024-07-08 17:07:45 -0500

    run pre-commit

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,48 @@
 import logging
 import os
+from datetime import datetime
 from threading import Thread
+from typing import Tuple
 
+import depot
 import pytest
-from depot import Depot
+from flask import Flask
+from testcontainers.rabbitmq import RabbitMqContainer
 from werkzeug.serving import make_server
 
+from queueclient import RabbitMQClient
 from queueclient.queue_factory import QueueFactory
 
 logging.basicConfig(level=logging.ERROR)
+logger = logging.getLogger(__name__)
+
+
+def _make_depot_app():
+    app = Flask(__name__)
+    # it is important that setup_default_handlers is called before
+    # setting up more specific error handlers so that we don't
+    # override them
+    depot.setup_default_handlers(app)
+    app.register_error_handler(500, depot.server_error)
+    app.register_blueprint(depot.v0, name="v0", url_prefix="/v0")
+    app.register_blueprint(depot.v0, name="v0_latest", url_prefix="/latest")
+    app.add_url_rule("/", "root", depot.root)
+    return app
+
+
+class _Depot:
+    def __init__(self, settings=None, debug=False):
+        """"""
+        self.app = _make_depot_app()
+
+        self.settings = settings
+        self.app.queue_data = {}
+        self.app.start_time = datetime.now()
+        self.app.version = "vt"
+        logger.info("Initializing depot at {}".format(self.app.start_time))
+
+    def run(self, *args, **kwargs):
+        self.app.run(*args, **kwargs)
 
 
 class DepotServer(Thread):
@@ -19,7 +53,7 @@ class DepotServer(Thread):
 
     def __init__(self):
         Thread.__init__(self)
-        self.depot = Depot()
+        self.depot = _Depot()
         self.depot.app.testing = True
         self.client = self.depot.app.test_client()
         self.server = make_server("localhost", 5000, self.depot.app)
@@ -49,17 +83,32 @@ def depot_fixture(request):
 
 
 @pytest.fixture()
-def rmq_fixture():
-    rbmq_host = os.environ.get("RABBITMQ_SERVER", "localhost")
-    rbmq_vhost = os.environ.get("RABBITMQ_VHOST", "/")
-    rbmq_qid = os.environ.get("RABBITMQ_QUEUE", "xtest")
-    q1 = QueueFactory.get_rabbitmq_client(
-        queue_id=rbmq_qid, host=rbmq_host, vhost=rbmq_vhost, durable=False
-    )
-    q2 = QueueFactory.get_rabbitmq_client(
-        queue_id=rbmq_qid, host=rbmq_host, vhost=rbmq_vhost, durable=False
-    )
-    yield q1, q2
+def rmq_fixture() -> Tuple[RabbitMQClient, RabbitMQClient]:
+    """RabbitMQ's initialization pytest fixture.
 
-    q1.close()
-    q2.close()
+    Starts a rabbitmq docker container using testcontainers.
+    """
+
+    with RabbitMqContainer("rabbitmq:3.13.1", username="guest", password="guest") as rabbitmq:
+        cl = rabbitmq.get_connection_params()
+        rbmq_host = os.environ.get("RABBITMQ_SERVER", rabbitmq.get_container_host_ip())
+        rbmq_vhost = os.environ.get("RABBITMQ_VHOST", cl.virtual_host)
+        rbmq_qid = os.environ.get("RABBITMQ_QUEUE", "xtest")
+        q1 = QueueFactory.get_rabbitmq_client(
+            queue_id=rbmq_qid,
+            host=rbmq_host,
+            vhost=rbmq_vhost,
+            port=rabbitmq.get_exposed_port(rabbitmq.RABBITMQ_NODE_PORT),
+            durable=False,
+        )
+        q2 = QueueFactory.get_rabbitmq_client(
+            queue_id=rbmq_qid,
+            host=rbmq_host,
+            vhost=rbmq_vhost,
+            port=rabbitmq.get_exposed_port(rabbitmq.RABBITMQ_NODE_PORT),
+            durable=False,
+        )
+        yield q1, q2
+
+        q1.close()
+        q2.close()
--- tests/test_queues.py
+++ tests/test_queues.py
@@ -1,7 +1,9 @@
 import json
 import uuid
 from threading import Thread
+from typing import Tuple
 
+from queueclient import RabbitMQClient
 from queueclient.core import InMemoryQueueClient
 from queueclient.depot import DepotQueueClient
 from queueclient.queue_factory import QueueFactory
@@ -59,7 +61,7 @@ def test_inmemory_queue_listening():
 
 
 def test_inmemory_queue_completes():
-    """When the queue is empty, calling close should should exit"""
+    """When the queue is empty, calling close should exit"""
     q = InMemoryQueueClient(str(uuid.uuid4()))
 
     def listen_for_messages():
@@ -118,7 +120,7 @@ def test_depot_queue_listening(depot_fixture):
     q.consume(callback=consume)
 
 
-def test_rabbitmq_queue(rmq_fixture):
+def test_rabbitmq_queue(rmq_fixture: Tuple[RabbitMQClient, RabbitMQClient]) -> None:
     """Tests reading and writing to supported queue types"""
 
     q, qx = rmq_fixture
@@ -137,7 +139,7 @@ def test_rabbitmq_queue(rmq_fixture):
     qx.consume(consumer_callback, requeue_failed=False)
 
 
-def test_rabbitmq_on_failure_callback(rmq_fixture):
+def test_rabbitmq_on_failure_callback(rmq_fixture: Tuple[RabbitMQClient, RabbitMQClient]) -> None:
 
     q, qx = rmq_fixture
 
@@ -158,7 +160,7 @@ def test_rabbitmq_on_failure_callback(rmq_fixture):
     qx.consume(consumer_callback, requeue_failed=False, on_failure_callback=f_call)
 
 
-def test_rabbitmq_deque(rmq_fixture):
+def test_rabbitmq_deque(rmq_fixture: Tuple[RabbitMQClient, RabbitMQClient]) -> None:
     q, qx = rmq_fixture
 
     response = q.enqueue(msg=dict(did="AAAAA", size=123))
