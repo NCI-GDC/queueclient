c1b1bcb0df9c8ec71973ff8b7e0125bbafc2b178
Jason Stiles <99777116+stilesj-uchicago@users.noreply.github.com>
2026-08-14 10:51:17 -0400

    DEV-4234 set heartbeat to maximum value to support large file processing (#35)

--- src/queueclient/rabbitmq.py
+++ src/queueclient/rabbitmq.py
@@ -13,6 +13,8 @@ from queueclient import core
 
 logger = logging.getLogger(__name__)
 
+RABBITMQ_MAX_ALLOWED_HEARTBEAT_SEC = 65535
+
 
 def should_retry(e: BaseException) -> bool:
     """Handles all the connection errors that may occur."""
@@ -82,8 +84,9 @@ class RabbitMQClient(core.QueueClient):
         routing_key=None,
         # We have long running jobs (terabyte sized transfer) which blocks the heartbeats.
         # A failed heartbeat will cause the server to drop the client.
-        # Don't use a short heartbeat unless you need it.
-        heartbeat=3600,
+        # The largest file so far is 2.2TB which takes 14 hours to process with `inspector`.
+        # The maximum allowed heartbeat is just over 18 hours.
+        heartbeat=RABBITMQ_MAX_ALLOWED_HEARTBEAT_SEC,
     ):
         """
         Args:
